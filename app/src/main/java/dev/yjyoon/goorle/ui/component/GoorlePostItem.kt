351c17efdd3fa69e9ae44e06890ed20050d5f153
yjyoon-dev <yjyoon.dev@gmail.com>
2023-05-28 16:28:21 +0900

    feat: post item 공통 컴포넌트 구현

--- app/src/main/java/dev/yjyoon/goorle/ui/component/GoorlePostItem.kt
+++ app/src/main/java/dev/yjyoon/goorle/ui/component/GoorlePostItem.kt
@@ -1,4 +1,63 @@
 package dev.yjyoon.goorle.ui.component
 
-class GoorlePostItem {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import dev.yjyoon.goorle.R
+import dev.yjyoon.goorle.ui.post.Post
+import dev.yjyoon.goorle.ui.theme.GoorleGray9E
+
+@Composable
+fun GoorlePostItem(
+    post: Post
+) {
+    Column {
+        AsyncImage(
+            model = post.thumbnail,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(4 / 3f)
+                .clip(RoundedCornerShape(24.dp))
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(text = post.title, style = MaterialTheme.typography.titleMedium)
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_location_on),
+                    contentDescription = null,
+                    tint = GoorleGray9E,
+                    modifier = Modifier.size(16.dp)
+                )
+                Spacer(modifier = Modifier.width(2.dp))
+                Text(
+                    text = post.location,
+                    style = MaterialTheme.typography.titleSmall,
+                    color = GoorleGray9E
+                )
+            }
+        }
+    }
 }
