d1757dfdde8a2b2b3328a337172159c2b6809308
yjyoon-dev <yjyoon.dev@gmail.com>
2023-05-28 16:28:03 +0900

    feat: divider 공동 컴포넌트 구현

--- app/src/main/java/dev/yjyoon/goorle/ui/component/GoorleDivider.kt
+++ app/src/main/java/dev/yjyoon/goorle/ui/component/GoorleDivider.kt
@@ -1,4 +1,19 @@
 package dev.yjyoon.goorle.ui.component
 
-class GoorleDivider {
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.yjyoon.goorle.ui.theme.GoorleGrayE0
+
+@Composable
+fun GoorleDivider(
+    modifier: Modifier = Modifier
+) {
+    Divider(
+        modifier = modifier.then(Modifier.fillMaxWidth()),
+        thickness = (0.75).dp,
+        color = GoorleGrayE0
+    )
 }
