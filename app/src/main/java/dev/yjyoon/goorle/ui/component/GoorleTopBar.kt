265fdd6e1ca0d95a2d7f374c0ad6e8ece1e69e3e
yjyoon-dev <yjyoon.dev@gmail.com>
2023-05-28 16:28:52 +0900

    feat: normal top bar 공통 컴포넌트 구현

--- app/src/main/java/dev/yjyoon/goorle/ui/component/GoorleTopBar.kt
+++ app/src/main/java/dev/yjyoon/goorle/ui/component/GoorleTopBar.kt
@@ -1,4 +1,63 @@
 package dev.yjyoon.goorle.ui.component
 
-class GoorleTopBar {
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import dev.yjyoon.goorle.R
+
+@Composable
+fun GoorleTopBar(
+    @StringRes titleRes: Int,
+    onBack: () -> Unit
+) {
+    val TOPBAR_HEIGHT = 56.dp
+
+    Surface(
+        modifier = Modifier
+            .height(TOPBAR_HEIGHT)
+            .fillMaxWidth(),
+        color = Color.White
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                onClick = onBack,
+                modifier = Modifier.size(24.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_arrow_back),
+                    contentDescription = null
+                )
+            }
+            Text(
+                text = stringResource(id = titleRes).replace('\n', ' '),
+                style = MaterialTheme.typography.titleLarge,
+                maxLines = 1
+            )
+            Spacer(modifier = Modifier.width(24.dp))
+        }
+    }
 }
