bfd72320f0cf6b2024d04247b60fc5cda6097a5e
yjyoon-dev <yjyoon.dev@gmail.com>
2023-05-28 16:27:24 +0900

    feat: 임시 post 데이터 클래스 정의

--- app/src/main/java/dev/yjyoon/goorle/ui/post/Post.kt
+++ app/src/main/java/dev/yjyoon/goorle/ui/post/Post.kt
@@ -1,4 +1,11 @@
 package dev.yjyoon.goorle.ui.post
 
-class Post {
-}
+import dev.yjyoon.goorle.ui.model.FilterType
+
+data class Post(
+    val id: Int = 0,
+    val thumbnail: String = "https://avatars.githubusercontent.com/u/72238126?v=4",
+    val title: String = "숙소 이름",
+    val location: String = "서울특별시 광진구",
+    val filters: List<FilterType> = listOf(FilterType.Parking, FilterType.Slope)
+)
