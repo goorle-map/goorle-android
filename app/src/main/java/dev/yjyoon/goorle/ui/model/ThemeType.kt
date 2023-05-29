9546e5fda1bbcd50603ab0b0af9b9c81f6baa8f9
yjyoon-dev <yjyoon.dev@gmail.com>
2023-05-30 05:30:41 +0900

    feat: 테마 별 태그 텍스트 정의

--- app/src/main/java/dev/yjyoon/goorle/ui/model/ThemeType.kt
+++ app/src/main/java/dev/yjyoon/goorle/ui/model/ThemeType.kt
@@ -6,13 +6,42 @@ import dev.yjyoon.goorle.R
 
 enum class ThemeType(
     @StringRes val stringRes: Int,
-    @DrawableRes val imageRes: Int
+    @StringRes val tagRes: Int,
+    @DrawableRes val imageRes: Int,
 ) {
-    Nature(stringRes = R.string.theme_nature, imageRes = R.drawable.img_home_sec_01),
-    Hanok(stringRes = R.string.theme_hanok, imageRes = R.drawable.img_home_sec_02),
-    Dog(stringRes = R.string.theme_dog, imageRes = R.drawable.img_home_sec_03),
-    Rural(stringRes = R.string.theme_rural, imageRes = R.drawable.img_home_sec_04),
-    Ocean(stringRes = R.string.theme_ocean, imageRes = R.drawable.img_home_sec_05),
-    Hotel(stringRes = R.string.theme_hotel, imageRes = R.drawable.img_home_sec_06),
-    Tour(stringRes = R.string.theme_tour, imageRes = R.drawable.img_home_sec_07),
+    Nature(
+        stringRes = R.string.theme_nature,
+        imageRes = R.drawable.img_home_sec_01,
+        tagRes = R.string.tag_nature
+    ),
+    Hanok(
+        stringRes = R.string.theme_hanok,
+        imageRes = R.drawable.img_home_sec_02,
+        tagRes = R.string.tag_hanok
+    ),
+    Dog(
+        stringRes = R.string.theme_dog,
+        imageRes = R.drawable.img_home_sec_03,
+        tagRes = R.string.tag_dog
+    ),
+    Rural(
+        stringRes = R.string.theme_rural,
+        imageRes = R.drawable.img_home_sec_04,
+        tagRes = R.string.tag_rural
+    ),
+    Ocean(
+        stringRes = R.string.theme_ocean,
+        imageRes = R.drawable.img_home_sec_05,
+        tagRes = R.string.tag_ocean
+    ),
+    Hotel(
+        stringRes = R.string.theme_hotel,
+        imageRes = R.drawable.img_home_sec_06,
+        tagRes = R.string.tag_hotel
+    ),
+    Tour(
+        stringRes = R.string.theme_tour,
+        imageRes = R.drawable.img_home_sec_07,
+        tagRes = R.string.tag_tour
+    ),
 }
