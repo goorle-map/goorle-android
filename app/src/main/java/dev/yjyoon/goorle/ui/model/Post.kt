0a5997c8a1de6f7a043f85b66e2804c9ff392811
yjyoon-dev <yjyoon.dev@gmail.com>
2023-05-30 05:29:56 +0900

    fix: post 임시 모델 구조 수정

---
+++ app/src/main/java/dev/yjyoon/goorle/ui/model/Post.kt
@@ -0,0 +1,13 @@
+package dev.yjyoon.goorle.ui.model
+
+import java.io.Serializable
+
+data class Post(
+    val id: Int = 0,
+    val images: List<String> = List(5) { "https://avatars.githubusercontent.com/u/72238126?v=4" },
+    val title: String = "숙소 이름",
+    val location: String = "서울특별시 광진구",
+    val filters: List<FilterType> = listOf(FilterType.Parking, FilterType.Slope),
+    val tags: List<ThemeType> = listOf(ThemeType.Hanok, ThemeType.Nature),
+    val comments: List<Comment> = List(5) { Comment() }
+) : Serializable
