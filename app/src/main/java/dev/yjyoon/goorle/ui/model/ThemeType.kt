1553b8db047f72cebd57ea67dbb79c2c32cd9830
yjyoon-dev <yjyoon.dev@gmail.com>
2023-05-29 13:47:10 +0900

    feat: string key 리팩토링 및 추가

--- app/src/main/java/dev/yjyoon/goorle/ui/model/ThemeType.kt
+++ app/src/main/java/dev/yjyoon/goorle/ui/model/ThemeType.kt
@@ -8,11 +8,11 @@ enum class ThemeType(
     @StringRes val stringRes: Int,
     @DrawableRes val imageRes: Int
 ) {
-    Nature(stringRes = R.string.home_section_item_01, imageRes = R.drawable.img_home_sec_01),
-    Hanok(stringRes = R.string.home_section_item_02, imageRes = R.drawable.img_home_sec_02),
-    Dog(stringRes = R.string.home_section_item_03, imageRes = R.drawable.img_home_sec_03),
-    Rural(stringRes = R.string.home_section_item_04, imageRes = R.drawable.img_home_sec_04),
-    Ocean(stringRes = R.string.home_section_item_05, imageRes = R.drawable.img_home_sec_05),
-    Hotel(stringRes = R.string.home_section_item_06, imageRes = R.drawable.img_home_sec_06),
-    Tour(stringRes = R.string.home_section_item_07, imageRes = R.drawable.img_home_sec_07),
+    Nature(stringRes = R.string.theme_nature, imageRes = R.drawable.img_home_sec_01),
+    Hanok(stringRes = R.string.theme_hanok, imageRes = R.drawable.img_home_sec_02),
+    Dog(stringRes = R.string.theme_dog, imageRes = R.drawable.img_home_sec_03),
+    Rural(stringRes = R.string.theme_rural, imageRes = R.drawable.img_home_sec_04),
+    Ocean(stringRes = R.string.theme_ocean, imageRes = R.drawable.img_home_sec_05),
+    Hotel(stringRes = R.string.theme_hotel, imageRes = R.drawable.img_home_sec_06),
+    Tour(stringRes = R.string.theme_tour, imageRes = R.drawable.img_home_sec_07),
 }
