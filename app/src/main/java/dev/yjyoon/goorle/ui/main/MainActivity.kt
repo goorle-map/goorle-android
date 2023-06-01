87671e097ac26f6729f4066812c2a414a8cf5f4e
yjyoon-dev <yjyoon.dev@gmail.com>
2023-06-02 01:02:37 +0900

    feat: hilt 적용

--- app/src/main/java/dev/yjyoon/goorle/ui/main/MainActivity.kt
+++ app/src/main/java/dev/yjyoon/goorle/ui/main/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import dev.yjyoon.goorle.ui.post.GridActivity
 import dev.yjyoon.goorle.ui.post.GridViewType
 import dev.yjyoon.goorle.ui.theme.GoorleTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private val viewModel: MainViewModel by viewModels()
