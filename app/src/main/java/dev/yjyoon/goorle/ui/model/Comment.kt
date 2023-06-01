066066ffd04bc883fa3e5c53c716d74af62fb346
yjyoon-dev <yjyoon.dev@gmail.com>
2023-06-02 01:07:30 +0900

    feat: comment 임시 데이터 구조 수정

--- app/src/main/java/dev/yjyoon/goorle/ui/model/Comment.kt
+++ app/src/main/java/dev/yjyoon/goorle/ui/model/Comment.kt
@@ -5,5 +5,8 @@ import java.io.Serializable
 data class Comment(
     val nickname: String = "구르리",
     val date: String = "23.04.20",
-    val content: String = "댓글을 이렇게 저렇게 달 수 있다."
+    val content: String = "댓글을 이렇게 저렇게 달 수 있다.",
+    val image: String = "https://avatars.githubusercontent.com/u/72238126?v=4",
+    val title: String = "호텔 전주점",
+    val location: String = "부산광역시 해운대구"
 ) : Serializable
