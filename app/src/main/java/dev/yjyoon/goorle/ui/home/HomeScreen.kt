7d1124017038fb47b6dd7eb6a64545f28c764778
yjyoon-dev <yjyoon.dev@gmail.com>
2023-06-02 01:09:25 +0900

    feat: topappbar를 mainscreen -> homescreen

--- app/src/main/java/dev/yjyoon/goorle/ui/home/HomeScreen.kt
+++ app/src/main/java/dev/yjyoon/goorle/ui/home/HomeScreen.kt
@@ -1,7 +1,10 @@
 package dev.yjyoon.goorle.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,10 +15,18 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import dev.yjyoon.goorle.R
 import dev.yjyoon.goorle.ui.component.GoorleSection
@@ -27,6 +38,7 @@ import dev.yjyoon.goorle.ui.model.RegionType
 import dev.yjyoon.goorle.ui.model.ThemeType
 import dev.yjyoon.goorle.ui.post.GridViewType
 import dev.yjyoon.goorle.ui.theme.GoorleBlack
+import dev.yjyoon.goorle.ui.theme.GoorleBlue
 
 @Composable
 fun HomeScreen(
@@ -34,116 +46,183 @@ fun HomeScreen(
 ) {
     val scrollState = rememberScrollState()
 
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Column(
-            modifier = Modifier.verticalScroll(scrollState)
+    Scaffold(
+        topBar = {
+            HomeTopBar(
+                onClickSearchBar = {}
+            )
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
         ) {
-            Spacer(modifier = Modifier.height(24.dp))
-            GoorleSection(
-                titleRes = R.string.home_section_title_01,
-                subtitleRes = R.string.home_section_subtitle_01,
-                onTap = { navigateToGrid(GridViewType.Theme) }
+            Column(
+                modifier = Modifier.verticalScroll(scrollState)
             ) {
-                Spacer(modifier = Modifier.width(8.dp))
-                ThemeType.values().forEach {
-                    GoorleSectionTileA(
-                        imageRes = it.imageRes,
-                        textRes = it.stringRes,
-                        onClick = { }
-                    )
+                Spacer(modifier = Modifier.height(24.dp))
+                GoorleSection(
+                    titleRes = R.string.home_section_title_01,
+                    subtitleRes = R.string.home_section_subtitle_01,
+                    onTap = { navigateToGrid(GridViewType.Theme) }
+                ) {
+                    Spacer(modifier = Modifier.width(8.dp))
+                    ThemeType.values().forEach {
+                        GoorleSectionTileA(
+                            imageRes = it.imageRes,
+                            textRes = it.stringRes,
+                            onClick = { }
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
                 }
-                Spacer(modifier = Modifier.width(8.dp))
-            }
-            Spacer(modifier = Modifier.height(18.dp))
-            GoorleSection(
-                titleRes = R.string.home_section_title_02,
-                subtitleRes = R.string.home_section_subtitle_02,
-                onTap = { navigateToGrid(GridViewType.Region) }
-            ) {
-                Spacer(modifier = Modifier.width(8.dp))
-                RegionType.values().forEach {
-                    GoorleSectionTileB(
-                        modifier = Modifier.size(156.dp),
-                        imageRes = it.imageRes,
-                        textRes = it.stringRes,
+                Spacer(modifier = Modifier.height(18.dp))
+                GoorleSection(
+                    titleRes = R.string.home_section_title_02,
+                    subtitleRes = R.string.home_section_subtitle_02,
+                    onTap = { navigateToGrid(GridViewType.Region) }
+                ) {
+                    Spacer(modifier = Modifier.width(8.dp))
+                    RegionType.values().forEach {
+                        GoorleSectionTileB(
+                            modifier = Modifier.size(156.dp),
+                            imageRes = it.imageRes,
+                            textRes = it.stringRes,
+                            onClick = {}
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+                Spacer(modifier = Modifier.height(18.dp))
+                GoorleSection(
+                    titleRes = R.string.home_section_title_03,
+                    subtitleRes = R.string.home_section_subtitle_03,
+                    onTap = { }
+                ) {
+                    Spacer(modifier = Modifier.width(8.dp))
+                    GoorleSectionTileC(
+                        color = Color(0xFFADDEFF),
                         onClick = {}
                     )
+                    GoorleSectionTileC(
+                        color = Color(0xFFC2FFAD),
+                        onClick = {}
+                    )
+                    GoorleSectionTileC(
+                        color = Color(0xFFC9ADFF),
+                        onClick = {}
+                    )
+                    GoorleSectionTileC(
+                        color = Color(0xFFFFCEAD),
+                        onClick = {}
+                    )
+                    GoorleSectionTileC(
+                        color = Color(0xFFADDEFF),
+                        onClick = {}
+                    )
+                    GoorleSectionTileC(
+                        color = Color(0xFFC2FFAD),
+                        onClick = {}
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
                 }
-                Spacer(modifier = Modifier.width(8.dp))
-            }
-            Spacer(modifier = Modifier.height(18.dp))
-            GoorleSection(
-                titleRes = R.string.home_section_title_03,
-                subtitleRes = R.string.home_section_subtitle_03,
-                onTap = { }
-            ) {
-                Spacer(modifier = Modifier.width(8.dp))
-                GoorleSectionTileC(
-                    color = Color(0xFFADDEFF),
-                    onClick = {}
-                )
-                GoorleSectionTileC(
-                    color = Color(0xFFC2FFAD),
-                    onClick = {}
-                )
-                GoorleSectionTileC(
-                    color = Color(0xFFC9ADFF),
-                    onClick = {}
-                )
-                GoorleSectionTileC(
-                    color = Color(0xFFFFCEAD),
-                    onClick = {}
-                )
-                GoorleSectionTileC(
-                    color = Color(0xFFADDEFF),
-                    onClick = {}
-                )
-                GoorleSectionTileC(
-                    color = Color(0xFFC2FFAD),
-                    onClick = {}
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-            }
-            Spacer(modifier = Modifier.height(18.dp))
-            GoorleSection(
-                titleRes = R.string.home_section_title_04,
-                subtitleRes = R.string.home_section_subtitle_04,
-                onTap = { /*TODO*/ }
-            ) {
-                Spacer(modifier = Modifier.width(8.dp))
-                GoorleSectionTileD(
-                    imageRes = R.drawable.img_comment_tile,
-                    onClick = {}
-                )
-                GoorleSectionTileD(
-                    imageRes = R.drawable.img_comment_tile,
-                    onClick = {}
-                )
-                GoorleSectionTileD(
-                    imageRes = R.drawable.img_comment_tile,
-                    onClick = {}
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-            }
-            Spacer(modifier = Modifier.height(12.dp))
-            Surface(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                shape = RoundedCornerShape(24.dp),
-                color = Color(0xFF9CFF7A)
-            ) {
+                Spacer(modifier = Modifier.height(18.dp))
                 GoorleSection(
-                    modifier = Modifier.padding(vertical = 8.dp),
-                    titleRes = R.string.home_section_title_05,
-                    subtitleRes = R.string.home_section_subtitle_05,
-                    textColor = GoorleBlack,
+                    titleRes = R.string.home_section_title_04,
+                    subtitleRes = R.string.home_section_subtitle_04,
                     onTap = { /*TODO*/ }
-                ) {}
+                ) {
+                    Spacer(modifier = Modifier.width(8.dp))
+                    GoorleSectionTileD(
+                        imageRes = R.drawable.img_comment_tile,
+                        onClick = {}
+                    )
+                    GoorleSectionTileD(
+                        imageRes = R.drawable.img_comment_tile,
+                        onClick = {}
+                    )
+                    GoorleSectionTileD(
+                        imageRes = R.drawable.img_comment_tile,
+                        onClick = {}
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+                Spacer(modifier = Modifier.height(12.dp))
+                Surface(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    shape = RoundedCornerShape(24.dp),
+                    color = Color(0xFF9CFF7A)
+                ) {
+                    GoorleSection(
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        titleRes = R.string.home_section_title_05,
+                        subtitleRes = R.string.home_section_subtitle_05,
+                        textColor = GoorleBlack,
+                        onTap = { /*TODO*/ }
+                    ) {}
+                }
+                Spacer(modifier = Modifier.height(12.dp))
             }
-            Spacer(modifier = Modifier.height(12.dp))
         }
     }
 }
+
+@Composable
+private fun HomeTopBar(
+    onClickSearchBar: () -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .height(HomeTopBarHeightExtended)
+            .fillMaxWidth(),
+        color = GoorleBlue,
+        shape = RoundedCornerShape(bottomStart = 24.dp, bottomEnd = 24.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            Image(
+                painter = painterResource(id = R.drawable.img_logo_white),
+                contentDescription = null,
+                modifier = Modifier.width(100.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .clickable { onClickSearchBar() },
+                color = Color.White,
+                shape = RoundedCornerShape(24.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(vertical = 12.dp, horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_search),
+                        contentDescription = null,
+                        tint = GoorleBlue,
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = stringResource(id = R.string.home_search_bar_placeholder),
+                        style = MaterialTheme.typography.titleLarge,
+                        color = Color(0xFF9E9E9E)
+                    )
+                }
+            }
+        }
+    }
+}
+
+private val HomeTopBarHeightExtended: Dp = 164.dp
--- app/src/main/java/dev/yjyoon/goorle/ui/main/MainScreen.kt
+++ app/src/main/java/dev/yjyoon/goorle/ui/main/MainScreen.kt
@@ -2,7 +2,6 @@ package dev.yjyoon.goorle.ui.main
 
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -23,7 +22,6 @@ import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -34,7 +32,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -77,11 +74,7 @@ fun MainScreen(
         }
     ) {
         Scaffold(
-            topBar = {
-                HomeTopBar(
-                    onClickSearchBar = {}
-                )
-            },
+
             bottomBar = {
                 MainNavigationBar(
                     currentDestination = currentDestination,
@@ -130,61 +123,6 @@ fun MainScreen(
     }
 }
 
-@Composable
-private fun HomeTopBar(
-    onClickSearchBar: () -> Unit
-) {
-    Surface(
-        modifier = Modifier
-            .height(HomeTopBarHeightExtended)
-            .fillMaxWidth(),
-        color = GoorleBlue,
-        shape = RoundedCornerShape(bottomStart = 24.dp, bottomEnd = 24.dp)
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-        ) {
-            Spacer(modifier = Modifier.weight(1f))
-            Image(
-                painter = painterResource(id = R.drawable.img_logo_white),
-                contentDescription = null,
-                modifier = Modifier.width(100.dp)
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Surface(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(48.dp)
-                    .clickable { onClickSearchBar() },
-                color = Color.White,
-                shape = RoundedCornerShape(24.dp)
-            ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(vertical = 12.dp, horizontal = 16.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_search),
-                        contentDescription = null,
-                        tint = GoorleBlue,
-                        modifier = Modifier.size(24.dp)
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(
-                        text = stringResource(id = R.string.home_search_bar_placeholder),
-                        style = MaterialTheme.typography.titleLarge,
-                        color = Color(0xFF9E9E9E)
-                    )
-                }
-            }
-        }
-
-    }
-}
 
 @Composable
 private fun MainNavigationBar(
@@ -269,4 +207,3 @@ private fun NavigationBarItem(
 }
 
 private val NavigationBarHeight: Dp = 58.dp
-private val HomeTopBarHeightExtended: Dp = 164.dp
