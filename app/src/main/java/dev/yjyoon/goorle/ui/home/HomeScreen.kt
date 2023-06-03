4f1945a341009c4254a5dfcca62c0743e2017f2a
yjyoon-dev <yjyoon.dev@gmail.com>
2023-06-04 01:57:41 +0900

    feat: 홈 화면 textfield 검색 바 구현

--- app/src/main/java/dev/yjyoon/goorle/ui/home/HomeScreen.kt
+++ app/src/main/java/dev/yjyoon/goorle/ui/home/HomeScreen.kt
@@ -1,7 +1,6 @@
 package dev.yjyoon.goorle.ui.home
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,6 +13,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +21,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -174,6 +178,8 @@ fun HomeScreen(
 private fun HomeTopBar(
     onClickSearchBar: () -> Unit
 ) {
+    var keyword by remember { mutableStateOf("") }
+
     Surface(
         modifier = Modifier
             .height(HomeTopBarHeightExtended)
@@ -190,37 +196,51 @@ private fun HomeTopBar(
             Image(
                 painter = painterResource(id = R.drawable.img_logo_white),
                 contentDescription = null,
+
                 modifier = Modifier.width(100.dp)
             )
             Spacer(modifier = Modifier.height(16.dp))
-            Surface(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(48.dp)
-                    .clickable { onClickSearchBar() },
-                color = Color.White,
-                shape = RoundedCornerShape(24.dp)
-            ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(vertical = 12.dp, horizontal = 16.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_search),
-                        contentDescription = null,
-                        tint = GoorleBlue,
-                        modifier = Modifier.size(24.dp)
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(
-                        text = stringResource(id = R.string.home_search_bar_placeholder),
-                        style = MaterialTheme.typography.titleLarge,
-                        color = Color(0xFF9E9E9E)
-                    )
+            BasicTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = keyword,
+                textStyle = MaterialTheme.typography.titleLarge.copy(color = Color(0xFF9E9E9E)),
+                onValueChange = { keyword = it },
+                singleLine = true,
+                decorationBox = { innerTextField ->
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(48.dp),
+                        color = Color.White,
+                        shape = RoundedCornerShape(24.dp)
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(vertical = 12.dp, horizontal = 16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_search),
+                                contentDescription = null,
+                                tint = GoorleBlue,
+                                modifier = Modifier.size(24.dp)
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Box(modifier = Modifier.weight(1f)) {
+                                if (keyword.isEmpty()) {
+                                    Text(
+                                        text = stringResource(id = R.string.home_search_bar_placeholder),
+                                        style = MaterialTheme.typography.titleLarge,
+                                        color = Color(0xFF9E9E9E)
+                                    )
+                                }
+                                innerTextField()
+                            }
+                        }
+                    }
                 }
-            }
+            )
         }
     }
 }
