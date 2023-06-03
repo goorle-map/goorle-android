2413489e6f08989ac7ae707a42052985d8801480
yjyoon-dev <yjyoon.dev@gmail.com>
2023-06-04 03:01:36 +0900

    feat: 패키지 여행 임시 모델 정의

---
+++ app/src/main/java/dev/yjyoon/goorle/ui/model/Trip.kt
@@ -0,0 +1,8 @@
+package dev.yjyoon.goorle.ui.model
+
+data class Trip(
+    val title: String = "초록여행",
+    val content: String = "장애인과 가족, 친구가 함께 원하는 곳을 자유롭게 여행 할 수 있는 장애인 여행이 바로 기아 초록여행 입니다.",
+    val imageUrl: String = "https://avatars.githubusercontent.com/u/72238126?v=4",
+    val url: String = "https://google.com"
+)
