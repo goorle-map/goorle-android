671eba1a1ee51b16d79bb630fe8738405a1fb7ba
yjyoon-dev <yjyoon.dev@gmail.com>
2023-06-08 02:35:05 +0900

    fix: position response 필드 네임 변경

--- app/src/main/java/dev/yjyoon/goorle/data/model/PositionResponse.kt
+++ app/src/main/java/dev/yjyoon/goorle/data/model/PositionResponse.kt
@@ -1,10 +1,6 @@
 package dev.yjyoon.goorle.data.model
 
-import com.google.gson.annotations.SerializedName
-
 data class PositionResponse(
-    @SerializedName("lat")
     val lat: Double,
-    @SerializedName("lon")
     val lng: Double
 )
