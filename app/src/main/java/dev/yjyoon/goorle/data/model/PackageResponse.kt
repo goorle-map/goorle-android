ba9c355df1519b50091d5dafec9ee7200f96dbda
yjyoon-dev <yjyoon.dev@gmail.com>
2023-06-08 02:35:17 +0900

    feat: package response 모델 정의

--- app/src/main/java/dev/yjyoon/goorle/data/model/PackageResponse.kt
+++ app/src/main/java/dev/yjyoon/goorle/data/model/PackageResponse.kt
@@ -5,4 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class PackageResponse(
     @SerializedName("packageId")
     val id: String,
+    val name: String,
+    val content: String,
+    val imageUrl: String,
+    val url: String
 )
