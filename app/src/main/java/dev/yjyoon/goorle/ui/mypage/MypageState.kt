da6e5899472886344cf1d1eba77a6ebc7f6a0ffd
yjyoon-dev <yjyoon.dev@gmail.com>
2023-06-10 16:22:35 +0900

    fix: 더미 닉네임 변경

--- app/src/main/java/dev/yjyoon/goorle/ui/mypage/MypageState.kt
+++ app/src/main/java/dev/yjyoon/goorle/ui/mypage/MypageState.kt
@@ -4,7 +4,7 @@ import dev.yjyoon.goorle.ui.model.Comment
 import dev.yjyoon.goorle.ui.model.Post
 
 data class MypageState(
-    val nickname: String = "떠돌이휴가중",
+    val nickname: String = "여섯자닉네임",
     val posts: List<Post> = emptyList(),
     val comments: List<Comment> = emptyList()
 )
